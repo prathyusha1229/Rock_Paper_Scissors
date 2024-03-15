e3968ecf285ce2179e96f01b2cf533677a01e379
Sai Prathyusha Pamulapati <119445710+prathyusha1229@users.noreply.github.com>
2024-03-15 14:45:59 -0500

    Add files via upload

---
+++ RPS.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import random
+
+game={1:"Rock",2:"Paper", 3: "Scissors"}
+choice=int(input("Choose a number[1:Rock,2:Paper, 3: Scissors]:"))
+
+computer=random.randint(1,3)
+print(f"{game[computer]}")
+
+if choice==computer:
+    print("draw")
+elif (choice==1 and computer==3) or (choice==3 and computer==2) or (choice==2 and computer==1):
+    print("You Won!")
+else:
+    print("OOPS, Computer Won")
+
